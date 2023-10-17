a2f4f199863cba8d76d1a861331e4790b4adfc98
fertittad <David.Fertitta@hmhco.com>
2023-10-17 10:02:38 -0500

    sheet js reading excel input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import ExcelReader from './components/ExcelReader';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>Math Excel Thingy</h1>
+            <ExcelReader />
+        </div>
+    );
 }
 
 export default App;

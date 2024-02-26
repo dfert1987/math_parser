cac447a37a674f392bdfaf17b43c99d5f4429fce
fertittad <David.Fertitta@hmhco.com>
2024-02-26 12:31:35 -0600

    favicon and name updates. homepage updates too

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,21 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+// import { useNavigate, useLocation } from 'react-router-dom';
 
 function NavBar() {
-    const navigate = useNavigate();
-    const location = useLocation();
+    // const navigate = useNavigate();
+    // const location = useLocation();
 
-    const pathMatchRoute = (route) => {
-        if (route === location.pathname) {
-            return true;
-        }
-    };
+    // const pathMatchRoute = (route) => {
+    //     if (route === location.pathname) {
+    //         return true;
+    //     }
+    // };
 
     return (
         <div className='topnav'>
-            <h4 className='navTitle'>Steve's Math Thing</h4>
+            <h4 className='navTitle'>Math Resource Finder</h4>
             <ul className='navListItems'>
-                <li
-                    className={
-                        pathMatchRoute('/')
-                            ? 'navListItem selected'
-                            : 'navListItem'
-                    }
-                    onClick={() => navigate('/')}>
-                    Upload
-                </li>
-                <li
-                    className={
-                        pathMatchRoute('/lessons')
-                            ? 'navListItem selected'
-                            : 'navListItem'
-                    }
-                    onClick={() => navigate('/lessons')}>
-                    Lessons
-                </li>
+                <li className={'navListItem'}>Find Your Lesson Below</li>
             </ul>
         </div>
     );
